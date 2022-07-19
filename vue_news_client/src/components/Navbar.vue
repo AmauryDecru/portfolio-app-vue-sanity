55aaa6e50013548f935e8b7e6a0f717ba59616c5
AmauryDecru <amaury.decru@student.howest.be>
2022-07-19 12:26:36 +0200

    Add dropdown styling

---
+++ vue_news_client/src/components/Navbar.vue
@@ -0,0 +1,53 @@
+<template>
+<header class="flex w-full items-center justify-center p-4">
+  <div :class="`menu-toggle relative z-50 ${menu_is_active ? 'is-active':''}`" @click="ToggleDropDown">
+    <div class="dropdown">
+      <span></span>
+    </div>
+
+  </div>
+
+  <h1 class="text center text-2x1 uppercase font-light tracking-widest">
+    BSF Mission Announcements
+  </h1>
+</header>
+</template>
+
+<script>
+import {computed} from "vue"
+import {useStore} from 'vuex'
+
+export default {
+  setup(){
+    const store = useStore()
+
+    const ToggleDropDown = () => {
+      store.dispatch('ToggleDropDown')
+    }
+
+    return{
+      menu_is_active: computed(() => store.state.menu_is_active),
+      ToggleDropDown
+    }
+  },
+  name: "Navbar-vue"
+}
+</script>
+
+<style scoped>
+.dropdown-toggle{
+  position:absolute;
+  top: 1rem;
+  left: 1rem;
+  width: 32px;
+  height: 32px;
+  cursor: pointer;
+}
+
+.dropdown{
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+</style>
--- vue_news_client/src/store/index.js
+++ vue_news_client/src/store/index.js
@@ -2,12 +2,30 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    menu_is_active: false,
+    anouncements: [],
+    authors: [],
+    total_anouncements: 0
   },
   getters: {
   },
   mutations: {
+    TOGGLE_DROPDOWN (state, dir = null){
+      if (dir === 'open'){
+        state.menu_is_active = true
+      }
+      else if (dir === 'close') {
+        state.menu_is_active = false
+      }
+      else {
+        state.menu_is_active = !state.menu_is_active
+      }
+    }
   },
   actions: {
+    ToggleDropDown ({commit}){
+      commit('TOGGLE_DROPDOWN')
+    }
   },
   modules: {
   }
