fd82d2e5645e462e28ad6e7812985c30eb6f645e
AmauryDecru <amaury.decru@student.howest.be>
2022-08-01 13:33:09 +0200

    Add Authors info to AnnouncementDetailView

--- vue_news_client/src/views/AnnouncementDetailView.vue
+++ vue_news_client/src/views/AnnouncementDetailView.vue
@@ -5,8 +5,16 @@
       <button @click="$router.back()" class="flex items-center text-lg text-red-500 hover:text-red-600 duration-300 mb-4">
         <span class="material-icons text-lg mr-1">keyboard_double_arrow_left</span> Return
       </button>
-      <h1 class="text=3xl md:text-5xl mb-8">{{ announcement.title }}</h1>
-      <p></p>
+      <h1 class="text=3xl md:text-5xl mb-8 font-Electrolize">{{ announcement.title }}</h1>
+      <p class="text-white text-lg font-bold mb-8">{{ announcement.summary }}</p>
+      <p v-html="TextReformatter(announcement.content)" class="mb-8"></p>
+      <div class="flex items-center mb-4">
+        <img :src="CreateURL(announcement.author.profile_picture, 300, 300)" class="block rounded-full w-12 h-12 mr-4">
+        <div>
+          <h1 class="text-gray-500 text-lg">{{ announcement.author.username }}</h1>
+          <p class="text-gray-500 text-sm">{{ FormatDate(announcement._createdAt) }}</p>
+        </div>
+      </div>
     </section>
     <section v-else>
       <p class="text-white italic text-2xl">Loading ...</p>
@@ -18,7 +26,7 @@
 import {onMounted, ref} from 'vue'
 import {useRoute} from 'vue-router'
 import sanity from '../client'
-import {CreateURL} from "@/utilities";
+import {FormatDate, CreateURL, TextReformatter} from "@/utilities";
 
 export default {
   setup(){
@@ -37,7 +45,9 @@ export default {
 
     return{
       announcement,
-      CreateURL
+      FormatDate,
+      CreateURL,
+      TextReformatter
     }
   }
 }
